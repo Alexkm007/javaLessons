d624486d38713e11fd8940c8acba20b486c9547b
Alexkm007 <koschelev@gamil.com>
2020-08-17 11:43:11 +0300

    new step

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -3,12 +3,10 @@ package com.javarush.task.task24.task2413;
 public class Ball extends BaseObject{
     @Override
     public void move() {
-
     }
 
     @Override
     public void draw(Canvas canvas) {
-
     }
 
     public Ball(double x, double y, double radius) {
