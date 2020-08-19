57c7fab4deb0880a3b894bedf662e64315e8abc6
Alexkm007 <koschelev@gamil.com>
2020-08-19 16:46:20 +0300

    new step

---
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task36.task3608.model;
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.List;
+
+public class FakeModel implements Model{
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    public void loadUsers(){
+       List<User> users =  modelData.getUsers();
+        users.add(new User("A", 1, 1));
+        users.add(new User("B", 2, 1));
+    }
+
+}
