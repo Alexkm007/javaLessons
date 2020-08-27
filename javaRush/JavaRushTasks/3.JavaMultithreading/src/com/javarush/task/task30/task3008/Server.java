5b2e80bf4950466fcdcb53f5539c6c24e0e756b3
Alexkm007 <koschelev@gamil.com>
2020-08-27 15:16:53 +0300

    task3008

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,33 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.Proxy;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Server is ready");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
 
     }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
 }
