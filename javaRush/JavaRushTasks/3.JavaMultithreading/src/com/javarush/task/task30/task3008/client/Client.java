519deca7b150d0099a200fd66bb4075c21ea9b45
Alexkm007 <koschelev@gamil.com>
2020-08-30 22:46:02 +0300

    task3008

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -6,6 +6,7 @@ import com.javarush.task.task30.task3008.Message;
 import com.javarush.task.task30.task3008.MessageType;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
 
@@ -127,6 +128,21 @@ public class Client {
             }
         }
 
+        @Override
+        public void run() {
+
+            try {
+                String nameServer = getServerAddress();
+                int port  = getServerPort();
+                Socket socket = new Socket(nameServer,port);
+                Client.this.connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            } catch (IOException | ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
+            }
+
+        }
     }
 }
 
