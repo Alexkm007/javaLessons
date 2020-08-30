9dc8e496bd4b2fe5c5500fd68fb8e238162005d4
Alexkm007 <koschelev@gamil.com>
2020-08-30 22:37:42 +0300

    task3008

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -79,6 +79,35 @@ public class Client {
 
     public class SocketThread extends Thread {
 
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            Message message = connection.receive();
+            while (message.getType() == MessageType.NAME_REQUEST) {
+                Message outMessage = new Message(MessageType.USER_NAME, getUserName());
+                connection.send(outMessage);
+                message = connection.receive();
+            }
+            if (message.getType() == MessageType.NAME_ACCEPTED) {
+                notifyConnectionStatusChanged(true);
+                return;
+            }
+            throw new IOException("Unexpected MessageType");
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    processIncomingMessage(message.getData());
+                } else if (message.getType() == MessageType.USER_ADDED) {
+                    informAboutAddingNewUser(message.getData());
+                } else if (message.getType() == MessageType.USER_REMOVED) {
+                    informAboutDeletingNewUser(message.getData());
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
         protected void processIncomingMessage(String message) {
             ConsoleHelper.writeMessage(message);
         }
@@ -100,3 +129,4 @@ public class Client {
 
     }
 }
+
