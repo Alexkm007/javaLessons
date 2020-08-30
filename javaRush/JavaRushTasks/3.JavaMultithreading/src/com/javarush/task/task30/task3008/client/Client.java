ccaf0fd7c781f5f037c4e8030575c27066fde1d7
Alexkm007 <koschelev@gamil.com>
2020-08-30 17:41:16 +0300

    task3008

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -12,6 +12,11 @@ public class Client {
     protected Connection connection;
     private volatile boolean clientConnected;
 
+    public static void main(String[] args) throws IOException {
+        Client client = new Client();
+        client.run();
+    }
+
     protected String getServerAddress() throws IOException {
         return ConsoleHelper.readString();
     }
@@ -42,9 +47,37 @@ public class Client {
 
     }
 
+    public  void run() throws IOException {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this){
+            try {
+                wait();
+                if(clientConnected){
+                    ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+                } else {
+                    ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+                }
+
+                while (clientConnected){
+                    String s  = ConsoleHelper.readString();
+                    if(s.equals("exit")){
+                        break;
+                    }
+                    if(shouldSendTextFromConsole()){
+                    connection.send(new Message(MessageType.TEXT,s));}
+                }
+            } catch (InterruptedException e) {
+               ConsoleHelper.writeMessage(e.getMessage());
+               return;
+            }
+        }
+
+    }
+
+
     public class SocketThread extends Thread {
-
-
 
     }
 }
