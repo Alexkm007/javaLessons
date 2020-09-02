641e6da8840e7282ff86c6d5d47a0ebbbd4fe891
Alexkm007 <koschelev@gamil.com>
2020-09-02 16:59:34 +0300

    3110

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,9 +1,11 @@
 package com.javarush.task.task31.task3110;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 
 public class ZipFileManager {
@@ -13,7 +15,21 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
 
-    public void createZip(Path source) throws Exception{
+    public void createZip(Path source) throws Exception {
+
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream is = Files.newInputStream(source)) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            int i = is.available();
+            byte[] buffer = new byte[i];
+            is.read(buffer);
+            zipOutputStream.write(buffer);
+            zipOutputStream.closeEntry();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
 
     }
 
