bb1b032d81be4c0c730a21241161b4b1dcb9c747
Alexkm007 <koschelev@gamil.com>
2020-09-03 21:05:17 +0300

    2812

---
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+    public static String readString() throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        return rd.readLine();
+    }
+
+    public static int readInt() throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(rd.readLine());
+    }
+
+
+}
