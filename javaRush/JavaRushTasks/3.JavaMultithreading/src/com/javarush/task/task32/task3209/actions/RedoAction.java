b5c7425991c82d124cf140c4c0f159aa672b2dec
Alexkm007 <koschelev@gamil.com>
2020-09-21 18:31:18 +0300

    task 3209 part 12

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/RedoAction.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/RedoAction.java
@@ -8,11 +8,10 @@ import java.awt.event.ActionEvent;
 public class RedoAction extends AbstractAction {
     private View view;
 
-    public RedoAction(View view) {
-        this.view = view;
-    }
+    public RedoAction(View view) {this.view = view;}
+
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        view.redo();
     }
 }
