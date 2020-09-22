8b5b59b2d3357e8dfa585ea3e338e06f4735d813
Alexkm007 <koschelev@gamil.com>
2020-09-22 10:24:05 +0300

    task 3209 part 16

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -3,6 +3,8 @@ package com.javarush.task.task32.task3209;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
     private View view;
@@ -22,6 +24,28 @@ public class Controller {
     }
 
     public void init(){
+    }
+
+    //записывает переданный текст с html тегами в документ document
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader stringReader = new StringReader(text);
+        try {
+            new HTMLEditorKit().read(stringReader, document, 0);
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    // получает текст из документа со всеми html тегами.
+    public String getPlainText() {
+        StringWriter stringWriter = new StringWriter();
+        try {
+            new HTMLEditorKit().write(stringWriter, document, 0, document.getLength());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+        return stringWriter.toString();
     }
 
     // удаляет существующий документ и создает пустой
