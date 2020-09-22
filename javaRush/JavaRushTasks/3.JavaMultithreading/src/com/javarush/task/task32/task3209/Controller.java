baf18bfa29de2e61f57f1b248b691a7e9190b1cc
Alexkm007 <koschelev@gamil.com>
2020-09-22 09:46:09 +0300

    task 3209 part 15

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task32.task3209;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 public class Controller {
@@ -13,19 +14,27 @@ public class Controller {
     }
 
     public static void main(String[] args) {
-
         View view = new View();
         Controller controller = new Controller(view);
         view.setController(controller);
         view.init();
         controller.init();
-        
     }
 
     public void init(){
-
     }
 
+    // удаляет существующий документ и создает пустой
+    public void resetDocument() {
+        if (document != null) {
+            // удаляет существующий документ
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        // создает документ по умолчанию
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
+    }
 
     //геттер для документа
     public HTMLDocument getDocument() {
