9c1c6547dd19a11e71a64a93289dabe426942bf9
Alexkm007 <koschelev@gamil.com>
2020-09-22 10:36:08 +0300

    task 3209 part 19

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,10 +1,9 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.*;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
-import java.io.File;
-import java.io.StringReader;
-import java.io.StringWriter;
+import java.io.*;
 
 public class Controller {
     private View view;
@@ -46,6 +45,69 @@ public class Controller {
             ExceptionHandler.log(e);
         }
         return stringWriter.toString();
+    }
+
+    public void saveDocument() {
+        if (currentFile == null) saveDocumentAs();
+        else {
+            view.selectHtmlTab();
+            view.setTitle(currentFile.getName());
+
+            try {
+                FileWriter fileWriter = new FileWriter(currentFile);
+                new HTMLEditorKit().write(fileWriter, document, 0, document.getLength());
+            } catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+
+        }
+    }
+
+    public void createNewDocument() {
+        view.selectHtmlTab();
+        resetDocument();
+        view.setTitle("HTML редактор");
+        currentFile = null;
+        view.resetUndo();
+
+    }
+
+    // открыть документ
+    public void openDocument() {
+//        view.selectHtmlTab();
+//        JFileChooser jFileChooser = new JFileChooser();
+//        jFileChooser.setFileFilter(new HTMLFileFilter());
+//        int choose = jFileChooser.showOpenDialog(view);
+//        if (choose == JFileChooser.APPROVE_OPTION) {
+//            currentFile = jFileChooser.getSelectedFile();
+//            resetDocument();
+//            view.setTitle(currentFile.getName());
+//            try {
+//                FileReader fileReader = new FileReader(currentFile);
+//                new HTMLEditorKit().read(fileReader, document, 0);
+//                view.resetUndo();
+//
+//            } catch (Exception e) {
+//                ExceptionHandler.log(e);
+//            }
+//        }
+    }
+    //сохранить документ как..
+    public void saveDocumentAs() {
+//        view.selectHtmlTab();
+//        JFileChooser jFileChooser = new JFileChooser();
+//        jFileChooser.setFileFilter(new HTMLFileFilter());
+//        int choose = jFileChooser.showSaveDialog(view);
+//        if (choose == JFileChooser.APPROVE_OPTION) {
+//            currentFile = jFileChooser.getSelectedFile();
+//            view.setTitle(currentFile.getName());
+//            try {
+//                FileWriter fileWriter = new FileWriter(currentFile);
+//                new HTMLEditorKit().write(fileWriter, document, 0, document.getLength());
+//            } catch (Exception e) {
+//                ExceptionHandler.log(e);
+//            }
+//        }
     }
 
     // удаляет существующий документ и создает пустой
