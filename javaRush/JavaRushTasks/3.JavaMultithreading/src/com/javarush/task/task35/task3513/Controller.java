a7572875c6398f808828a61cab0005a8a8a533bb
Alexkm007 <koschelev@gamil.com>
2020-09-30 10:40:14 +0300

    task 3513 part 9

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
     private Model model;
@@ -10,7 +11,7 @@ public class Controller extends KeyAdapter {
     // конструктор, устанавливает модель и вид для контроллера
     public Controller(Model model) {
         this.model = model;
-       // this.view = new View(this);
+        this.view = new View(this);
     }
 
     // геттер счета
@@ -21,6 +22,38 @@ public class Controller extends KeyAdapter {
     //геттер для поля
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
+    }
+
+    // сброс игры
+    public void resetGame() {
+        model.score = 0;
+        view.isGameLost = false;
+        view.isGameWon = false;
+        model.resetGameTiles();
+    }
+
+    // обработка нажатий на клавиатуре
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) resetGame();
+        else {
+            if (!model.canMove()) view.isGameLost = true;
+            else {
+                if (!view.isGameLost && !view.isGameWon) {
+                    switch (e.getKeyCode()) {
+                        case KeyEvent.VK_UP : model.up(); break;
+                        case KeyEvent.VK_DOWN : model.down(); break;
+                        case KeyEvent.VK_LEFT : model.left(); break;
+                        case KeyEvent.VK_RIGHT : model.right(); break;
+//                        case KeyEvent.VK_Z : model.rollback(); break;
+//                        case KeyEvent.VK_R : model.randomMove(); break;
+//                        case KeyEvent.VK_A : model.autoMove(); break;
+                    }
+                }
+                if (model.maxTile == WINNING_TILE) view.isGameWon = true;
+            }
+        }
+        view.repaint();
     }
 
 }
