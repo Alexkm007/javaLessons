f0f901a052839e223d1a775e9635463655d88c12
Alexkm007 <koschelev@gamil.com>
2020-10-04 21:08:43 +0300

    task 2712 part 7

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -6,7 +6,6 @@ public class Advertisement {
     private long initialAmount; //начальная сумма, стоимость рекламы в копейках
     private int hits; //количество оплаченных показов
     private int duration; //продолжительность в секундах
-    private long amountPerOneDisplaying;
 
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
@@ -14,6 +13,5 @@ public class Advertisement {
         this.initialAmount = initialAmount;
         this.hits = hits;
         this.duration = duration;
-        this.amountPerOneDisplaying = initialAmount / hits;
     }
 }
