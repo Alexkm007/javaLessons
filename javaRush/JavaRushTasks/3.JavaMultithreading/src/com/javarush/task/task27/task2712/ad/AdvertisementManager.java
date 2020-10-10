7c30e9f12e7057695497f458d45a90e2e21a2dfd
Alexkm007 <koschelev@gamil.com>
2020-10-10 20:07:15 +0300

    task 2515

--- javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ javaRush/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -2,6 +2,8 @@ package com.javarush.task.task27.task2712.ad;
 
 import com.javarush.task.task27.task2712.ConsoleHelper;
 
+import java.util.List;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     private int timeSeconds;
@@ -11,6 +13,8 @@ public class AdvertisementManager {
     }
 
     public void processVideos() {
-        ConsoleHelper.writeMessage("calling processVideos method");
+        List<Advertisement> advertisements = storage.list();
+        if (advertisements.isEmpty())
+            throw new NoVideoAvailableException();
     }
 }
