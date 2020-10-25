b6d07557abe55faf3b0c0d8a349de107c87a904e
Alexkm007 <koschelev@gamil.com>
2020-10-25 17:40:47 +0300

    task 2810 part 6

--- javaRush/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ javaRush/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +12,10 @@ public class HHStrategy implements Strategy{
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
 
     @Override
-    public List<Vacancy> getVacancies(String searchString) {
+    public List<Vacancy> getVacancies(String searchString) throws IOException {
+        String url = String.format("%s?text=%s&page=%s",URL_FORMAT, searchString, 2);
+        Document doc = Jsoup.connect(url).get();
+
         return new ArrayList<Vacancy>();
     }
 }
