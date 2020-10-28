703b06d8c07952571d8030302d0ff64b5291a4dd
Alexkm007 <koschelev@gamil.com>
2020-10-28 19:27:45 +0300

    task 3810 part 16

--- javaRush/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ javaRush/JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.HHStrategy;
-import com.javarush.task.task28.task2810.model.Model;
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.model.Strategy;
+import com.javarush.task.task28.task2810.model.*;
 import com.javarush.task.task28.task2810.view.HtmlView;
 import com.javarush.task.task28.task2810.view.View;
 
@@ -14,8 +11,9 @@ public class Aggregator {
     public static void main(String[] args) throws IOException {
 
         Provider provider = new Provider(new HHStrategy());
+        Provider moikrugProvider = new Provider(new MoikrugStrategy());
         HtmlView view = new HtmlView();
-        Model model = new Model(view,provider);
+        Model model = new Model(view, provider, moikrugProvider);
         Controller controller = new Controller(model);
         view.setController(controller);
         view.userCitySelectEmulationMethod();
