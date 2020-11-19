1ddfc662961307ec4ac8a7f9a1abf5e3c928828c
Alexkm007 <koschelev@gamil.com>
2020-11-19 20:32:06 +0300

    задание 4-ч1

--- springcourse/anton_mentor/src/homework4/ArreySumm.java
+++ springcourse/anton_mentor/src/homework4/ArreySumm.java
@@ -1,5 +1,11 @@
 package homework4;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.concurrent.*;
+
 public class ArreySumm {
 
     private int[] arrey;
@@ -15,19 +21,81 @@ public class ArreySumm {
         this.arrey = arrey;
     }
 
-    public void initialArrey(int i){
+    public void initialArrey(int i) {
         arrey = new int[i];
-        for(int j = 0;j <i;j++){
-            arrey[j] = (int)(Math.random() * 10);
+        for (int j = 0; j < i; j++) {
+            arrey[j] = (int) (Math.random() * 10);
         }
     }
 
-    public long summArrey(){
+    public long summAllArrey() {
         long x = 0;
-        for(int j:getArrey()){
-            x+=j;
+        for (int j : arrey) {
+            x += j;
         }
         return x;
     }
+
+    public long summPartArrey(int i, int j) {
+        long x = 0;
+        for (; i <= j; i++) {
+            x += arrey[i];
+        }
+        return x;
+    }
+
+    public long returnDurationOneThread() throws InterruptedException {
+        LocalDateTime start = LocalDateTime.now();
+        System.out.println("Сумма массива в однопотоке: " + summAllArrey());
+        LocalDateTime end = LocalDateTime.now();
+        Duration duration = Duration.between(start, end);
+
+        return duration.toMillis();
+    }
+
+
+    public long returnDurationMultiThread(int countThread) {
+
+        int indexStart = 0;
+        int step = arrey.length / countThread;
+        int indexEnd = indexStart + step;
+        List<Future<Long>> futureList = new ArrayList<>();
+        ExecutorService  executor = Executors.newFixedThreadPool(countThread);
+
+        LocalDateTime startTime = LocalDateTime.now();
+        for (int i = 1; i <= countThread; i++) {
+            int delta = arrey.length - indexEnd;
+            int start = indexStart;
+            int end   = indexEnd;
+
+            Callable task = new Callable() {
+                @Override
+                public Object call() throws Exception {
+                    return summPartArrey(Integer.valueOf(start), delta < step ? arrey.length - 1 : end);
+                }
+            };
+            futureList.add(executor.submit(task));
+
+            indexStart = indexEnd+1;
+            indexEnd +=step;
+
+        }
+        long k = 0;
+        for(Future<Long> fut : futureList){
+            try {
+                k+=fut.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        executor.shutdown();
+        System.out.println("Сумма массива в мультипотоке: "+k);
+        LocalDateTime endTime = LocalDateTime.now();
+        Duration duration = Duration.between(startTime, endTime);
+        return duration.toMillis();
+
+    }
+
 
 }
--- springcourse/anton_mentor/src/homework4/Main.java
+++ springcourse/anton_mentor/src/homework4/Main.java
@@ -3,9 +3,10 @@ package homework4;
 import test.A;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ArreySumm arreySumm = new ArreySumm();
-        arreySumm.initialArrey(100000000);
-        System.out.println(arreySumm.summArrey());
+        arreySumm.initialArrey(99999999);
+        System.out.println("Длительность в однопотоке: " + arreySumm.returnDurationOneThread() + "мс");
+        System.out.println("Длительность в многопотоке: " +arreySumm.returnDurationMultiThread(4) + "мс");
     }
 }
