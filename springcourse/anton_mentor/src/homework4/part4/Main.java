864ecd54fde803606c412984dbe89129c140026b
Alexkm007 <koschelev@gamil.com>
2020-11-22 19:37:57 +0300

    задание 4-ч4

---
+++ springcourse/anton_mentor/src/homework4/part4/Main.java
@@ -0,0 +1,9 @@
+package homework4.part4;
+
+public class Main {
+    public static void main(String[] args) {
+        WeitChenge weitChenge = new WeitChenge();
+        weitChenge.setPathChenges("C:\\test");
+        weitChenge.start();
+    }
+}
