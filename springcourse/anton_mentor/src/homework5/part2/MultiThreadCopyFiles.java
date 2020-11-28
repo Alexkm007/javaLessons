72c16197652bf981126916ae2cfd463f54229b27
Alexkm007 <koschelev@gamil.com>
2020-11-28 19:08:44 +0300

    задание 5-2- первая часть

--- springcourse/anton_mentor/src/homework5/part2/MultiThreadCopyFiles.java
+++ springcourse/anton_mentor/src/homework5/part2/MultiThreadCopyFiles.java
@@ -1,12 +1,17 @@
 package homework5.part2;
 
 import java.io.*;
+import java.nio.file.Paths;
+import java.text.Format;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MultiThreadCopyFiles {
     private String pathSource;
     private String pathDest;
-    private long progresByte = 0;
-    private byte[] buffer = new byte[1024*1024];
+    private volatile long progresByte = 0;
+    private volatile byte[] buffer = new byte[1024];
+    private ReentrantLock lock  = new ReentrantLock();
+    private volatile boolean isFileRead = false;
 
     public MultiThreadCopyFiles() {
     }
@@ -32,9 +37,49 @@ public class MultiThreadCopyFiles {
         this.pathDest = pathDest;
     }
 
+    public void writeFile(){
+        try (FileOutputStream os = new FileOutputStream(new File(pathDest))){
+            while (!isFileRead){
+            lock.lock();
+            os.write(buffer);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void printProgress(){
+
+        long size  = new File(pathSource).length();
+        while (!isFileRead){
+            float proz = (float) (progresByte/size*100.00);
+            String out = String.format("Выполнено -  %2.f! ",proz );
+            try {
+                System.out.write(out.getBytes());
+                Thread.sleep(500);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
     public void readFile(){
         try (FileInputStream is = new FileInputStream(new File(pathSource))){
-
+            while (is.read() != -1){
+                lock.lock();
+                if(is.available() < buffer.length){
+                    buffer = new byte[is.available()];
+                }
+                progresByte = progresByte + buffer.length;
+                is.read(buffer);
+                lock.unlock();
+            }
+            buffer = new byte[0];
+            isFileRead = true;
         } catch (Exception e) {
             e.printStackTrace();
         }
