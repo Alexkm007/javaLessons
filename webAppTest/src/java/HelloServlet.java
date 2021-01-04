89d26e186912e954dc46326b292559d85a3264c6
Alexkm07 <alexkm@inbox.ru>
2021-01-04 20:50:55 +0300

    servlet

--- webAppTest/src/java/HelloServlet.java
+++ webAppTest/src/java/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        PrintWriter printWriter =  resp.getWriter();
-        printWriter.write("Hello Word");
+       printWriter.write("Hello Word");
     }
 }
