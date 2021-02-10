051c8a5823821496b0e84bdb049a20b2792a6215
Alexkm07 <alexkm@inbox.ru>
2021-02-10 20:14:34 +0300

    Spring HW1-1

--- hibernate/springData/src/main/db/addcustomers.sql
+++
@@ -1,2 +0,0 @@
-insert into CUSTOMERS (NAME,ADDRESS,ID)  values ('Sasha','Kud ups 8',1),
-                                                ('Petya',' Set 22, ups',2);
--- hibernate/springData/src/main/java/ru/alexkm07/spdata/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.alexkm07.spdata.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import ru.alexkm07.spdata.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
