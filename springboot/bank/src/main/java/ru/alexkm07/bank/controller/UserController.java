b25b2b1c21d6d4919bac8976690fb06dd8dc8eed
Alexkm07 <alexkm@inbox.ru>
2021-02-16 22:22:58 +0300

    bank start 2

--- springboot/bank/src/main/java/ru/alexkm07/bank/controller/UserController.java
+++ springboot/bank/src/main/java/ru/alexkm07/bank/controller/UserController.java
@@ -4,29 +4,37 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.alexkm07.bank.model.User;
 import ru.alexkm07.bank.repository.UserRepository;
+import ru.alexkm07.bank.service.UserService;
 
 import java.util.List;
 
 @Service
 @RequestMapping("/user")
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping()
     public String getUserList(Model model){
 
-        List<User> users = userRepository.findAll();
+        List<User> users = userService.findAll();
         model.addAttribute("users",users);
 
         return "userlist";
     }
 
+    @GetMapping("edit")
+    public String getUser(Model model, @RequestParam("id") Long id){
+        User user = userService.getById(id);
+        model.addAttribute("user",user);
+        return "useredit";
+    }
 
 
 }
