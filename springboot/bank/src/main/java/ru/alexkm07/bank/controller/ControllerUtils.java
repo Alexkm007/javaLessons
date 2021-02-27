989235c57c9ba257c75b40651557310859f67755
Alexkm07 <alexkm@inbox.ru>
2021-02-27 22:34:04 +0300

    bank start 5

--- springboot/bank/src/main/java/ru/alexkm07/bank/controller/ControllerUtils.java
+++ springboot/bank/src/main/java/ru/alexkm07/bank/controller/ControllerUtils.java
@@ -4,13 +4,26 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ControllerUtils {
     static Model getErrors(BindingResult bindingResult, Model model) {
 
-        for(FieldError fieldError:bindingResult.getFieldErrors()){
-            model.addAttribute(fieldError.getField()+"Error",fieldError.getDefaultMessage());
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            model.addAttribute(fieldError.getField() + "Error", fieldError.getDefaultMessage());
         }
 
         return model;
     }
+
+
+    static String dateToString(Date date, String pattern){
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        return simpleDateFormat.format(date);
+
+    }
+
 }
+
