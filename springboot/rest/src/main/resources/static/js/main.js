784d51fa861097a133ece742c9edda2ddee2317b
Alexkm07 <alexkm@inbox.ru>
2021-04-05 22:50:53 +0300

    3 step rest app

--- springboot/rest/src/main/resources/static/js/main.js
+++ springboot/rest/src/main/resources/static/js/main.js
@@ -52,16 +52,24 @@ Vue.component('message-form',{
 });
 
 Vue.component('message-row',{
-    props: ['message', 'editMethod'],
+    props: ['message', 'editMethod', 'messages'],
     template:'<div>'+
     '<i>({{ message.id }})</i>{{ message.text }}'+
-    '<span>' +
+    '<span style="position: absolute; right:0">' +
         '<input type="button" value="Edit" @click="edit"/>' +
+        '<input type="button" value="X" @click="del"/>' +
     '</span>'+
     '</div>',
     methods: {
         edit: function(){
             this.editMethod(this.message);
+        },
+        del: function(){
+            messageApi.remove({id:this.message.id}).then(result =>{
+                if(result.ok){
+                    this.messages.splice(this.messages.indexOf(this.message),1)
+                }
+            })
         }
     }
 });
@@ -76,7 +84,8 @@ Vue.component('messages-list',{
     template:
         '<div>' +
         '<message-form :messages="messages" :messageAttr="message"/>'+
-        '<message-row v-for="message in messages" :key = "message.id" :message="message" :editMethod ="editMethod"/>'+
+        '<message-row v-for="message in messages" :key = "message.id" :message="message"'+
+            ':editMethod ="editMethod" :messages="messages"/>'+
         '</div>',
     created: function() {
     messageApi.get().then(result =>
